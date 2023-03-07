198cbb050c39511ad5c4ae3b7c42e9553d5f9e79
jc <jiglesia@student.42.fr>
2023-03-07 19:07:48 +0100

    node setup updated

--- node_setup.sh
+++ node_setup.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 
 sudo apt install npm
-git clone https://github.com/nvm-sh/nvm.git ~/nvm
-cd ~/nvm
-./install.sh
-source ~/.bashrc
-nvm install --lts
-npm install -g npm@latest
-npm install -g @angular/cli
-npm install
+curl https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
+echo "<==  Run  ==> 
+source ~/.zshrc && \
+nvm install --lts && \
+npm install -g npm@latest && \
+npm install -g @angular/cli && \
+npm install"
